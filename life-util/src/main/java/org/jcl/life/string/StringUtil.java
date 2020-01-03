f83067f2019f18ec3e537cd75d381d8cf90e3947
leizi <569421432@qq.com>
2020-01-03 14:52:08 +0800

    工具类修改Mod by liulei _20200103

--- life-util/src/main/java/org/jcl/life/string/StringUtil.java
+++ life-util/src/main/java/org/jcl/life/string/StringUtil.java
@@ -3,11 +3,11 @@ package org.jcl.life.string;
 final public class StringUtil {
 
 	/**
-	 * 判断字符串是否有效
-	 * @param str
+	 * 判断对象是否有效
+	 * @param obj
 	 * @return
 	 */
-	public static boolean isEmpty(final String str) {
-		return null == str || str.isEmpty();
+	public static boolean isEmpty(final Object obj) {
+		return null == obj || "".equals(obj);
 	}
 }
