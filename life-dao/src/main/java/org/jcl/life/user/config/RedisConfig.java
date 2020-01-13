3bc2ed001cd9c5de36aaa23dfeef2f13768ade21
chenglei <chenglei@4366.com>
2020-01-13 19:54:41 +0800

    life-dao\src\main\java\org\jcl\life\user\config\RedisConfig.java

--- life-dao/src/main/java/org/jcl/life/user/config/RedisConfig.java
+++ life-dao/src/main/java/org/jcl/life/user/config/RedisConfig.java
@@ -1,11 +1,9 @@
 package org.jcl.life.user.config;
 
 import org.springframework.cache.annotation.CachingConfigurerSupport;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-
 
 public class RedisConfig extends CachingConfigurerSupport {
 
